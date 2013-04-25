c25210b18bcd4a3a51acb66d4d9e79165c794167
Wing Ho Yeung <ho13102003@hotmail.com>
2013-04-25 21:46:07 +0800

    fb login

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <html>
 <body>
-	This is the start page of the bigtwo project.
-	This is the second page of the bigtwo project.<br>
-	This is the second line of the bigtwo project.
-	Testing from ON99
-	Testing from Gab
-	testing sheldon
+	<div class="fb-login-button" data-show-faces="true" data-width="200" data-max-rows="1"></div>
+	<div id="fb-root"></div>
 </body>
 </html>
+
+<script>
+var id = 123059651225050;
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/zh_HK/all.js#xfbml=1&appId=123059651225050";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+</script>
