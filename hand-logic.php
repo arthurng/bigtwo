64bbc3287a94be7693014620598db7e5e47908f6
Arthur Ng <arthurng.cy@gmail.com>
2013-05-10 19:52:13 +0800

    update threeCard() and improved the SQL query

--- hand-logic.php
+++ hand-logic.php
@@ -33,15 +33,15 @@ function oneCard($cards){
 	$currentHand = array("1", $cards[0]);
 
 	// 3. Check if "passed three times"
-	if (checkIfLastThreeIsPass()) {
-		saveNewHand(join("-", $currentHand));
+	if (checkIfLastThreeIsPass($r)) {
+		saveNewHand($r, join("-", $currentHand));
 		return true;
 	}
 
 	// 4. Chech if the hand is larger than the previous
 	if ($currentHand[1] < $firstLastValue[1]) return false;
 	else {
-		saveNewHand(join("-", $currentHand));
+		saveNewHand($r, join("-", $currentHand));
 		return true;
 	}
 }
@@ -60,26 +60,49 @@ function twoCard($cards){
 	}
 
 	// 3. Check if "passed three times"
-	if (checkIfLastThreeIsPass()) {
-		saveNewHand(join("-", $currentHand));
+	if (checkIfLastThreeIsPass($r)) {
+		saveNewHand($r, join("-", $currentHand));
 		return true;
 	}
 
 	// 4. Chech if the hand is larger than the previous
 	if ($currentHand[1] < $firstLastValue[1]) return false;
 	else {
-		saveNewHand(join("-", $currentHand));
+		saveNewHand($r, join("-", $currentHand));
 		return true;
 	}
 }
 
 function threeCard($cards){
 	$r = fetchLast();
-	// not implemented yet
+	$firstLastValue = split("-", $r["firstLast"]);
+
+	// 1. Check if previous hand consist of n cards
+	if ($firstLastValue[0] != 3 && $firstLastValue[0] != "PASS") return false;
+
+	// 2. Validate and calculate the current hand
+	if (ceil($cards[0]/4) != ceil($cards[1]/4) || ceil($cards[1]/4) != ceil($cards[2]/4)) return false;
+	else {
+		$currentHand = array("3", max($cards));
+	}
+
+	// 3. Check if "passed three times"
+	if (checkIfLastThreeIsPass($r)) {
+		saveNewHand($r, join("-", $currentHand));
+		return true;
+	}
+
+	// 4. Chech if the hand is larger than the previous
+	if ($currentHand[1] < $firstLastValue[1]) return false;
+	else {
+		saveNewHand($r, join("-", $currentHand));
+		return true;
+	}
 }
 
 function fiveCard($cards){
 	$r = fetchLast();
+	$firstLastValue = split("-", $r["firstLast"]);	
 	// not implemented yet
 }
 
@@ -94,21 +117,13 @@ function fetchLast(){
 	} else return $r;
 }
 
-function saveNewHand($newHand){
+function saveNewHand($r, $newHand){
 	$db = new PDO('mysql:host=www.shop151.ierg4210.org;dbname=bigtwo', "bigtwoadmin", "csci4140");
-	$q = $db -> prepare("SELECT firstLast, secondLast, thirdLast, forthLast FROM game WHERE sessionid = ? LIMIT 1");
-	$q->execute(array($_REQUEST["sessionid"]));
-	$r = $q->fetch();
-	if (!$r) {
-		error_log("Session not found!");
-	} else {
-		$q = $db -> prepare("UPDATE game SET firstLast = ?, secondLast = ?, thirdLast = ?, forthLast = ? WHERE sessionid = ?");
-		$q->execute(array($newHand, $r["firstLast"], $r["secondLast"], $r["thirdLast"], $_REQUEST["sessionid"]));
-	}
+	$q = $db -> prepare("UPDATE game SET firstLast = ?, secondLast = ?, thirdLast = ?, forthLast = ? WHERE sessionid = ?");
+	$q->execute(array($newHand, $r["firstLast"], $r["secondLast"], $r["thirdLast"], $_REQUEST["sessionid"]));
 }
 
-function checkIfLastThreeIsPass(){
-	$r = fetchLast();
+function checkIfLastThreeIsPass($r){
 	if (($r["firstLast"]=="PASS") && ($r["secondLast"]=="PASS") && ($r["thirdLast"]=="PASS")) return true;
 	else return false;
 }
@@ -130,6 +145,6 @@ function test2(){
 }
 
 test();
-*/
 
+*/
 ?>
