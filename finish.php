4d2cc64eda915791cefbe4b8a81df414ecc08cf8
Arthur Ng <arthurng.cy@gmail.com>
2013-05-22 18:06:08 +0800

    Add example of "endGame()" function
    include the script "endgame.js" and call endGame(roomid);

---
+++ finish.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+	Placeholder for the game
+</body>
+<script type="text/javascript" src="incl/jquery.js"></script>
+<script type="text/javascript" src="endgame.js"></script>
+<script type="text/javascript">
+	endGame(999);
+</script>
+</html>	
