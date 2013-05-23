fb2a084a428f11c3a50ae36e83a5ce9b1263b824
Wing Ho Yeung <ho13102003@hotmail.com>
2013-05-23 21:00:22 +0800

    DEBUGGING

--- hand-logic.php
+++ hand-logic.php
@@ -179,7 +179,7 @@ function fiveCard($cards){
 			}
 		}
 		// For case like "66555"
-		else if(ceil($cards[1]/4) != (ceil($cards[2]/4)){
+		else if(ceil($cards[1]/4) != (ceil($cards[2]/4))){
 			if(ceil($cards[0]/4) == ceil($cards[1]/4) && ceil($cards[2]/4) == ceil($cards[3]/4) && ceil($cards[3]/4) == ceil($cards[4]/4)){
 				$currentHand = array("6", $cards[4]));
 			}
