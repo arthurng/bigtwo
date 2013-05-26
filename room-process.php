43fe13c17c20b934148dc9f49008ae6236772842
sheldonip <ytt3562@gmail.com>
2013-05-26 18:22:14 +0900

    Update of createRoom()

--- room-process.php
+++ room-process.php
@@ -214,7 +214,7 @@
 		$r = $q->fetch();
 		if($r == null){
 			$q = $db -> prepare("INSERT INTO game (sessionid,roomid,cardnorth,cardeast,cardsouth,cardwest) VALUES(?,?)");
-			$q->execute(array($session,$roomid,0,0,0,0));
+			$q->execute(array($session,$roomid,'0','0','0','0'));
 		}
 		return true;
 	}
