2e260a5809d11639f6f130098b5f7773509702bb
Arthur Ng <arthurng.cy@gmail.com>
2013-05-28 17:49:25 +0800

    Update Set turn

--- room-process.php
+++ room-process.php
@@ -55,16 +55,17 @@
 		// Find out who holds diamond 3
 		if($hand[($cardNum/4-1)] == '1'){
 			// Translate player id to player name
-			// Bug fix
-			/*if($playerid == 0) {
-				$playerid = 3;
+			$player = id2player($playerid);
+			// Fix to cater for another bug.
+			switch($player){
+				case "north": $playerAdj = "west"; break;
+				case "west": $playerAdj = "south"; break;
+				case "south": $playerAdj = "east"; break;
+				case "east": $playerAdj = "north"; break;
 			}
-			else {
-				$playerid = $playerid - 1;
-			}*/
 			$player = id2player($playerid);
 			$q = $db -> prepare("UPDATE game SET turn = ? WHERE roomid = ?");
-			$q->execute(array($player,$roomid));
+			$q->execute(array($playerAdj,$roomid));
 		}
 		
 		// Store the card
